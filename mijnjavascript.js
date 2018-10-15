c1b0394d4304bb7908e2b7f5c120c1c09758829b
Denver666 <43335969+Denver666@users.noreply.github.com>
2018-10-15 09:35:07 +0200

    first

---
+++ mijnjavascript.js
@@ -0,0 +1,15 @@
+console.log("Hello World")
+
+var a = 10;
+for (var i = 0; i < 10; i++){
+    console.log("De waarde van i is " + i);
+}
+
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur:"blauw"
+}
+
+console.log(mijnauto.kleur);
